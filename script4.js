ab6bec046a0ee02e3e738da28ee1ce66a7764851
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-01-16 17:52:17 -0500

    updated javascript in script4.js to reflect the goals of the objective in the project.

--- script4.js
+++ script4.js
@@ -1,14 +1,20 @@
 ///////// Assignment Code
 var generateBtn = document.querySelector("#generate");
 //////////////////////////////////////////////
+
 ///////// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
+  //////////////My code/////////////////
+  function generatePassword(){
+      for(i=0; i <= password.length[7]; i++ )
+  }
 
 }
+//////////////////////////////////////////////
+
 ///////// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 ////////////////////////////////////////////////
