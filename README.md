b6d1421fb3f2866ef35eb9e3d3273e7463e6a522
BinaryBitBytes <94703967+BinaryBitBytes@users.noreply.github.com>
2022-01-19 18:15:23 -0500

    Updated Readme, updated script7.js

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Password-Generator
 This is a basic password generator. Enjoy!
+
+In this password generator I made an algorithm that assembles a random set of values plus a combination of a few arrays to make up the password itself. You can click the 'GENERATE PASSWORD' button to generate a password. I used math.random and math.floor to randomly select characters from their arrays to be used in the password.
+
+I just noticed in the readme I did not meet all acceptence criteria. Specifically with prompting the user and storing their values from the prompt. Ive searched around on the web and I need to find or figure a solution of how to restore what the user typed to put into logic to meet the selection based criteria.
+
+This password generator does generate upper case, lower case, numbers, symbols, and special characters in its password.
+
+There is no set character length for the entire password.
+There is no set selection method for choosing any specifics in choosing to make up the password requirements. I will have to resubmit this excercise to meet that criteria.
+
+If we can use a html form that would work better than on screen javascript prompts. 
--- script7.js
+++ script7.js
@@ -11,9 +11,19 @@ var SYMBOL = ['!','@','/','#','$','%','&','*','+','-','^'];//created an array of
 var SYMBOL2 = ['!','@','/','#','$','%','&','*','+','-','^'];//created an array of symbols for use in password , i made a duplicated list and called it symbol2 so the computer can generate a seperate vallue from array Symbol
 var SYMBOL3 = ['☺','☻','♥','♦','♣','♠','•','◘','○','♂','♀','♪','♫','☼','►','◄','↕','‼','¶','§','▬','↨','↑','↓','→','←','∟','↔','▲','▼'];//created an array of unique characters
 var DECI = ['1','2','3','4','5','6','7','8','9']; //created an array of string numbers
+
+alert('Welcome to my password generator.');
+var a = prompt('How many Characters do you want your password to be? Minimum 8 max, 128. Enter a number.');
+var b = prompt('Would you like to use upper-case, lower-case, or a combination of both characters in your password, type: upper , lower , or both?');
+var c = prompt('Would you like to use symbols and special characters in your password? If so type yes or if not then type no.');
+console.log(a,b,c); // this allows us to store the values of a b and c to answer the questions for minimum requirements
+/*
+Enter Updated code here
+*/
+
 function writePassword() {
   var password = generatePassword(); //parent variable liked to global function that generates password
-    function generatePassword(){ // defining the function below used to generate the password, this i child in the DOM within the genratePassword Function / variable password
+    function generatePassword(){ // defining the function below used to generate the password, this i child in the DOM within the genratePassword Function / variable password        
         var randomDec = DECI[Math.floor(Math.random()*DECI.length)]; //using math random to declare a variable to generate array values from the list containing decimal strings
         var randomWord1 = WORD1[Math.floor(Math.random()*WORD1.length)]; //using math random to declare a variable to generate array values from the list containing word strings list 1
         var randomWord2 = WORD2[Math.floor(Math.random()*WORD2.length)];//using math random to declare a variable to generate array values from the list containing word strings list 2
@@ -24,13 +34,13 @@ function writePassword() {
         var randomSymbol = SYMBOL[Math.floor(Math.random()*SYMBOL.length)];;//using math random to declare a variable to generate array values from the list containing symbols
         value = [((Math.floor(randomDec))*(Math.round(randomDec)*randomDec))+randomUpper+randomSymbol3+
             (randomDec*randomDec)+randomLower+randomSymbol+randomWord2+
-            randomSymbol3+randomUpper+randomWord1+randomUpper+randomDec+randomSymbol2]; //My password generating algorithm
+            randomSymbol3+randomUpper+randomWord1+randomUpper+randomDec+randomSymbol2];//My password generating algorithm
         return value; //wrapping VALUE to return to the generate password field
         }
         var passwordText = document.querySelector("#password"); // creates a variable to store the password in text and uses the document.queryselector to grab the password ID from the DOM in the HTML
-
         passwordText.value = password;/* saves password text from the document.querySelector ID password and liks this password to return to the
         generate password function so the password can be written in the box so the user can see it.*/
 }
+
 generateBtn.addEventListener("click", writePassword); /*when the generate button is click the event listener continues to call the write password function that then 
         calls the generate password function to be returned and written the the password generator box*/
